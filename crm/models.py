ec0a4edd42326dc3ecebf5cda78ea234f5fedb26
Nikita <dedushka-is-here@protonmail.com>
2022-10-23 17:50:26 +0300

    added somewhat...

---
+++ crm/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+# Create your models here.
+class Order(models.Model):
+    order_dt = models.DateTimeField(auto_now=True)
+    order_username = models.CharField(max_length=200, verbose_name="Name")
+    order_phone = models.CharField(max_length=200, verbose_name="Phone Number")
+
+    def __str__(self):
+        return self.order_username
+
+    class Meta:
+        verbose_name = "Order"
+        verbose_name_plural = "Orders"
